180a7b194a96299df61c4298b431ee5a4679647d
Qasim579 <kqasim579@gmail.com>
2022-09-28 10:03:38 +0500

    init

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 const express = require('express');
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+// const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,13 +20,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById('63242b72ecfe39f3b207d874')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -32,6 +33,22 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(4000);
-});
+mongoose.connect('mongodb+srv://qasim:qasim123@nodeapplication.me7nd.mongodb.net/shop').then(reusult => {
+  User.findOne().then(user => {
+    if (!user) {
+      const user = new User({
+        name: 'Qasim Ahmad',
+        email: 'qasim@ahmad.com',
+        cart: {
+          items: []
+        }
+      });
+      user.save();
+
+    }
+  })
+
+  app.listen(3000);
+}).catch(err => {
+  console.log(err)
+})
--- controllers/admin.js
+++ controllers/admin.js
@@ -15,7 +15,10 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl);
+  const product = new Product({
+    title: title, price: price, description: description, imageUrl: imageUrl, userId: req.user
+  });
+
   product
     .save()
     .then(result => {
@@ -57,15 +60,13 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc,
-    updatedImageUrl,
-    prodId 
-  );
-  product
-    .save()
+  Product.findById(prodId).then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.imageUrl = updatedImageUrl;
+    product.description = updatedDesc
+    return product.save();
+  })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -74,8 +75,11 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
+  // .select('title price -_id')
+  // .populate('userId','name')
     .then(products => {
+      console.log(products)
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
@@ -88,7 +92,7 @@ exports.getProducts = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
 
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
